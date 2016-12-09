6487f00fde03c1cd5ce7f02448b132886ec047d6
kvzhao <kelispinor@gmail.com>
2016-12-09 16:14:50 +0800

    complete hw 1-15

--- hw1/hw_1-15.py
+++ hw1/hw_1-15.py
@@ -1,9 +1,16 @@
 from mltools.PLA import PLA
+import numpy as np
 
+print ('Prob 1-15')
 data_path = 'data/hw1_15_train.dat'
-
 pla = PLA()
 
 pla.load_train_data(data_path)
-pla.init_weight(mode='zero')
-pla.train()
+mode = 'zero'
+pla.init_weight(mode)
+print ('Initialization method: ' + mode)
+
+num_of_updates = pla.train()
+print ('Updates Number: %d' % num_of_updates) 
+blame_index = np.argmax(pla.data_flawness)
+print ('Most frequent update: %d' % blame_index)
